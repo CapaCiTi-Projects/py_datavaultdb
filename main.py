683f44ba37c0d711948f333bd043952d056fca2f
Jesse <jesseb727@gmail.com>
2020-07-15 13:33:58 +0200

    Convert code into class-based with tkinter

--- main.py
+++ main.py
@@ -1,18 +1,141 @@
+from collections import OrderedDict
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from pandastable import Table
 
 import matplotlib
 import matplotlib.pyplot as plt
+import math
 import MySQLdb
+import os.path
 import pandas as pd
 import tkinter as tk
+import tkinter.font as tkfont
 import tkinter.filedialog
 
 
-matplotlib.use("TkAgg")
+class Application (tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.configure(background="#fff")
+
+        self.fonts = {
+            "title": tkfont.Font(family="Lucida Grande", size=24)
+        }
+
+        self.tabs = [
+            DataFrame,
+            StatsFrame
+        ]
+        self.create_widgets()
+        self.set_tab(0)
+
+    def create_widgets(self):
+        self.rowconfigure(index=2, weight=1)
+        self.columnconfigure(index=0, weight=1)
+
+        # Create title Widget
+        self.title_label = tk.Label(
+            self, text="DataVault Inc.", font=self.fonts["title"], justify="left", bg="#f0f0f0")
+        self.title_label.grid(row=0, column=0, ipady=8,
+                              ipadx=12, sticky="NSEW")
+
+        # Create view selection widgets, i.e. tab buttons
+        if len(self.tabs) > 1:
+            self.subheader_frame = tk.Frame(self, bg="#f0f0f0")
+            self.subheader_frame.grid(
+                row=1, column=0, ipady="8", sticky="NSEW")
+
+            # Set subheader_frame to have 8 columns.
+            for col in range(8):
+                self.subheader_frame.columnconfigure(index=col, weight=1)
+
+        # Create container for actual tabs.
+        self.tab_container = tk.Frame(self)
+        self.tab_container.grid(row=2, column=0, sticky="NSEW")
+
+        self.tab_buttons = []
+        for idx, tab in enumerate(self.tabs):
+            if len(self.tabs) > 1:
+                # Create tab buttons
+                t = tk.Button(self.subheader_frame, text=tab.label, relief="ridge",
+                              command=lambda index=idx: self.set_tab(index))
+
+                self.tab_buttons.append(t)
+                self.tab_buttons[idx].grid(ipadx=10, ipady=5, sticky="NSEW",
+                                           row=0, column=(4 - math.floor(len(self.tabs) / 2) + idx),
+                                           columnspan=(len(self.tabs) % 2 + 1))
+
+            # Create tab frames
+            self.tabs[idx] = tab(master=self.tab_container)
+            self.tabs[idx].grid(row=0, column=0, sticky="NSEW")
+
+    def set_tab(self, frame_idx):
+        for idx, _ in enumerate(self.tab_buttons):
+            if idx == frame_idx:
+                self.tab_buttons[idx]["state"] = "disabled"
+                self.tabs[idx].tkraise()
+            else:
+                self.tab_buttons[idx]["state"] = "normal"
+
+
+class DataFrame(tk.Frame):
+    label = "View Data"
+
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.create_widgets()
+
+    def create_widgets(self):
+        df_cols, db_data = get_db_data()
+        data_df = pd.DataFrame(db_data, columns=df_cols).set_index("ID")
+
+        self.data_table = Table(self, dataframe=data_df)
+        self.data_table.autoResizeColumns()
+        self.data_table.show()
+
+
+class StatsFrame(tk.Frame):
+    label = "View Stats"
+
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+
+        f = self.get_plot_data()
+        self.plt_show(f)
+
+    def plt_show(self, f):
+        """Method to add the matplotlib graph onto a tkinter window."""
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.draw()
+        self.plot_widget = canvas.get_tk_widget()
+        self.plot_widget.grid(row=0, column=0, sticky="NSEW")
+
+    def get_plot_data(self):
+        # Get a data from DB and import into pandas.DataFrame
+        df_cols, db_data = get_db_data()
+        products_df = pd.DataFrame(db_data, columns=df_cols).set_index("ID")
+
+        # Create the matplotlib figure and axes that will be used to display the graphs for the statistics.
+        fig = Figure(figsize=(15, 5), dpi=100)
+        ax1 = fig.add_subplot(1, 3, 1)
+        ax2 = fig.add_subplot(1, 3, 2)
+        ax3 = fig.add_subplot(1, 3, 3)
+
+        # Create different statistics and plot them the figure previously defined.
+        products_df.groupby(["Category"]).size().plot(ax=ax1, y="Stock Available", kind="bar", grid=True,
+                                                      title="Number of Items per Category")
+        products_df.groupby(["Category"]).sum().plot(ax=ax2, y="Stock Available", kind="bar", grid=True,
+                                                     title="Total Number of Products per Category")
+        products_df.groupby(["Category"]).mean().plot(ax=ax3, y="Stock Available", kind="bar", grid=True,
+                                                      title="Average Price of Products in Category")
+
+        return fig
 
 
 def get_db_data():
+    """ Method to get the data from the database and return it as a tuple consisting
+    of a list of the names of the columns and a list of the actualy data in tuple format."""
     db = MySQLdb.connect("localhost", "root", "2ZombiesEatBrains?")
     cursor = db.cursor()
 
@@ -26,49 +149,5 @@ def get_db_data():
     return cols, data
 
 
-def plt_show(f, root):
-    canvas = FigureCanvasTkAgg(f, root)
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=2, column=0, columnspan=12)
-
-
-def import_csv():
-    filename = tkinter.filedialog.askopenfilename()
-    df = pd.read_csv(filename)
-    return df
-
-
-# Get a data from DB and import into pandas.DataFrame
-df_cols, db_data = get_db_data()
-products_df = pd.DataFrame(db_data, columns=df_cols).set_index("ID")
-
-
-# Plot different datapoints from the DataFrame on the same figure.
-fig = Figure(figsize=(15, 5), dpi=100)
-ax1 = fig.add_subplot(1, 3, 1)
-ax2 = fig.add_subplot(1, 3, 2)
-ax3 = fig.add_subplot(1, 3, 3)
-
-products_df.groupby(["Category"]).size().plot(ax=ax1, y="Stock Available", kind="bar", grid=True,
-                                              title="Number of Items per Category")
-products_df.groupby(["Category"]).sum().plot(ax=ax2, y="Stock Available", kind="bar", grid=True,
-                                             title="Total Number of Products per Category")
-products_df.groupby(["Category"]).mean().plot(ax=ax3, y="Stock Available", kind="bar", grid=True,
-                                              title="Average Price of Products in Category")
-
-
-root = tk.Tk()
-
-main_frame = tk.Frame(root)
-main_frame.grid(row=0, column=0, padx=12, pady=12)
-
-for col in range(12):
-    main_frame.columnconfigure(col, weight=1)
-
-# File Location
-tk.Button(main_frame, text="Import a CSV", command=import_csv())\
-    .grid(row=0, column=8)
-
-plt_show(fig, root)
-
-root.mainloop()
+app = Application()
+app.mainloop()
